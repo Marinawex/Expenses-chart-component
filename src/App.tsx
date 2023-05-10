1d1054f9f64c1a36dd404f68840fd40cdedac5a1
marinawex <sheep803@gmail.com>
2023-05-10 19:01:56 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import './App.scss'
+import daysObj from './data.json'
+
+// interface SpendingPerDay {
+//   day: string;
+//   amount: number
+// }
+
+// type spendingByDays {
+//   SpendingPerDay[]
+// }
+
+
+function App() {
+  const balance: number = 921.48;
+  const monthTotal: number = 478.33;
+  const stat : number = 2.4;
+  const currentDay = new Date().toDateString().toLocaleLowerCase().slice(0,3)
+
+  
+
+  const spendingByDaysList = daysObj.map((dayObj) => {
+  
+    const dayColor = currentDay === dayObj.day ? 'cayn' : 'red';
+    return ( <div key={dayObj.day}><div className='container'><div className={`amount ${dayColor}`} style={{height:`${dayObj.amount}%`}}>{dayObj.amount}</div></div> <div className='day'>{dayObj.day}</div> </div>)
+  })
+  
+ 
+
+
+console.log((currentDay))
+
+  
+
+  
+
+ 
+  
+
+  return (
+    <>
+<div className='balance'> <div className='h2'><h2>My balance</h2> <div className='total-balance'>{`$${balance}`}</div> </div><div className='circles-container'><div className='circle white'></div> <div className='circle black'></div></div>
+  </div>
+ 
+<div className='spending'>
+  
+  <h1>Spending - Last 7 days</h1>
+  <div className='spending-week' >
+ {spendingByDaysList}
+  </div>
+
+<div className='total'>
+  <div className='month-total'>
+    <h3>Total this month</h3>
+    ${monthTotal}
+</div>
+<div>
+  <div className='stat'>
+<span>+{stat}%</span>
+from last month</div></div>
+</div>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
